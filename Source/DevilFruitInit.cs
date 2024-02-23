2b8f37c6f246f2cbdb968d71e14144705ce20168
Dexter Lowe <feldoh10@gmail.com>
2024-02-23 23:52:32 +0000

    Alt graphic for skill trainers

---
+++ Source/DevilFruitInit.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using Verse;
+
+namespace DevilFruit;
+
+[StaticConstructorOnStartup]
+public static class DevilFruitInit
+{
+    static DevilFruitInit()
+    {
+        foreach (ThingDef def in DefDatabase<ThingDef>.AllDefsListForReading)
+        {
+            if (def.GetCompProperties<CompProperties_Neurotrainer>() is not { skill: not null }) continue;
+            GraphicData graphicData = new();
+            graphicData.CopyFrom(def.graphicData);
+            graphicData.texPath = "Things/Item/Special/MechSerumSkilltrainer";
+            def.graphicData = graphicData;
+        }
+    }
+}
